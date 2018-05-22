9335d92d88369c726a6064d01c989b798e680035
Mouli1993 <35778350+Mouli1993@users.noreply.github.com>
2018-05-22 22:12:43 +0530

    Update TestingJ.java

--- TestingJ.java
+++ TestingJ.java
@@ -2,7 +2,7 @@ public class TestingJ {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, W");
+        System.out.println("Hello
     }
 
 }
